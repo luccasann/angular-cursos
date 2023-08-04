14d7f9f51d43177d980869408549bf6309de9618
luccasann <luccathesann@gmail.com>
2023-08-04 19:28:25 -0300

    Criamos a tabela de cursos dinamica

--- src/app/cursos/cursos-list/cursos-list.component.ts
+++ src/app/cursos/cursos-list/cursos-list.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Curso } from '../shared/curso.model';
 
 @Component({
   selector: 'app-cursos-list',
   templateUrl: './cursos-list.component.html',
   styleUrls: ['./cursos-list.component.scss']
 })
-export class CursosListComponent {
+export class CursosListComponent implements OnInit{
+
+
+  cursos: Curso[];
+
+  constructor(){
+
+
+  }
+
+  ngOnInit(): void {
+    
+    
+    this.getCursos();
+
+  }
+
+  getCursos(){
+
+    this.cursos = [
+      {
+        nome:'Angular',
+        thumb:'',
+        versao:16,
+      },
+
+      {
+        nome:'Php',
+        thumb:'',
+        versao:8,
+      },
+
+      {
+        nome:'Node',
+        thumb:'',
+        versao:10,
+      },
+
+    ]
+  }
+
+  showCursos(){
+
+    console.log(this.cursos);
+  }
+
 
 }
+
+
