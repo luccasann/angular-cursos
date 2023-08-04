5f97dbb88eceee425f0d6f30bc1f1d3c4e3e4fa5
luccasann <luccathesann@gmail.com>
2023-08-04 18:30:09 -0300

    iniciando modulo cursos

---
+++ src/app/cursos/cursos-list/cursos-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-list',
+  templateUrl: './cursos-list.component.html',
+  styleUrls: ['./cursos-list.component.scss']
+})
+export class CursosListComponent {
+
+}
